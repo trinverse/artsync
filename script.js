b241f04941ade479c0dbae98dcd9eed139597a77
Ashish Tyagi <ashishtyagi10@gmail.com>
2025-08-16 11:37:52 -0400

    feat: add contact us page

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ contactForm.addEventListener('submit', (e) => {
     alert('Thank you for your message! We will get back to you soon.');
     contactForm.reset();
 });
+
+const contactFormPage = document.getElementById('contact-form-page');
+
+if (contactFormPage) {
+    contactFormPage.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Thank you for your message! We will get back to you soon.');
+        contactFormPage.reset();
+    });
+}
